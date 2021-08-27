584ec42faf94a786dd9ec52452ea499aa22e9e79
iamOjas <ojasmishra01@gmail.com>
2021-08-27 20:13:55 +0530

    Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,27 @@
 import React, {
-  Component,
   useEffect,
   useState
 } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import BPTTokenSaleContract from "./contracts/BPTTokenSale.json";
+import BPTTokenContract from "./contracts/BPTToken.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 var App = () => {
-  const [storageValue, setStorageValue] = useState(undefined);
+  const [balance, setBalance] = useState(0);
   const [web3, setWeb3] = useState(undefined);
   const [accounts, setAccounts] = useState(undefined);
   const [Contract, setContract] = useState(undefined);
-  const [value, setValue] = useState("Type Anything");
+  const [tokenContract, setTokenContract] = useState(undefined);
+  const [numberOfToken, SetnumberOfToken] = useState(0);
+  const [tokenPrice,setTokenPrice] = useState("");
+  const [tokensSold,setTokensSold] = useState("");
+  const [totalTokens,setTotalTokens] = useState("");
+  const [button,toggle] = useState(true);
 
   useEffect(() => {
-    const init = async() => {
+    const init = async () => {
       try {
         // Get network provider and web3 instance.
         const web3 = await getWeb3();
@@ -26,20 +31,29 @@ var App = () => {
 
         // Get the contract instance.
         const networkId = await web3.eth.net.getId();
-        const deployedNetwork = SimpleStorageContract.networks[networkId];
-        console.log(deployedNetwork);
-        const instance = new web3.eth.Contract(
-          SimpleStorageContract.abi,
-          deployedNetwork && deployedNetwork.address,
+        const deployedNetwork = BPTTokenSaleContract.networks[networkId];
+        const deployedNetwork1 = BPTTokenContract.networks[networkId];
+        
+        const Tokeninstance = new web3.eth.Contract(
+          BPTTokenContract.abi,
+          deployedNetwork1 && deployedNetwork1.address,
+          {from:accounts[0]}
         );
+
+        const instance = new web3.eth.Contract(
+          BPTTokenSaleContract.abi,
+          deployedNetwork && deployedNetwork.address,
+          {from:accounts[0]}
+        );
+
+        
 
         // Set web3, accounts, and contract to the state, and then proceed with an
         // example of interacting with the contract's methods.
-        // this.setState({ web3, accounts, contract: instance }, this.runExample);
         setWeb3(web3);
         setAccounts(accounts);
+        setTokenContract(Tokeninstance);
         setContract(instance);
-
       } catch (error) {
         // Catch any errors for any of the above operations.
         alert(
@@ -52,55 +66,55 @@ var App = () => {
   }, [])
 
   useEffect(() => {
-    const init = async() => {
-        // Stores a given value, 5 by default.
-        
-        await Contract.methods.set(value).send({
-          from: accounts[0]
-        });
-
-        // Get the value from the contract to prove it worked.
-        const response = await Contract.methods.get().call();
-
-        // Update state with the result.
-
-        setStorageValue(response)
-      }
-      if (typeof web3 !== 'undefined' && typeof accounts !== 'undefined' && typeof Contract !== 'undefined') {
+    const init = async () => {
+      const balance = await tokenContract.methods.balanceOf(accounts[0]).call();
+      const tokenPrice = await Contract.methods.tokenPrice().call();
+      var tokenssold = await Contract.methods.tokensSold().call();
+      console.log(Contract._address);
+      const totaltokens = await tokenContract.methods.balanceOf(Contract._address).call();
+      setTotalTokens(totaltokens.toString());
+      setTokensSold(tokenssold);
+      setTokenPrice(tokenPrice.toString());
+      setBalance(balance.toString())
+    }
+    if (typeof web3 !== 'undefined' && typeof accounts !== 'undefined' && typeof Contract !== 'undefined' && typeof tokenContract !== 'undefined') {
       init();
     }
 
-  }, [web3, accounts, Contract])
+  }, [web3, accounts, Contract, tokenContract,button])
 
-  if(typeof web3 === 'undefined'){
+  if (typeof web3 === 'undefined') {
     return <div> Loading Web3, accounts, and contract... </div>;
   }
 
-  async function handleSubmit(event){
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    await Contract.methods.set(value).send({
-      from: accounts[0]
+    await Contract.methods.buyTokens(numberOfToken).send({
+      from: accounts[0],
+      value: tokenPrice*numberOfToken
     });
-
-    // Get the value from the contract to prove it worked.
-    const response = await Contract.methods.get().call();
-
-    // Update state with the result.
-
-    setStorageValue(response)
+    
+    toggle(!button);
   }
-  
+
 
   return(
     <div className="App" >
-    <h1> {storageValue} </h1>
+      <h1>BLOCKCHAIN PRACTICE TOKEN SALE</h1>
+      <p>You currently have {balance} tokens</p>
+      <p>Token Price is {web3.utils.fromWei(tokenPrice, 'ether')} ether</p>
     <form onSubmit = {handleSubmit} >
-      <input type="text" 
-      onChange = {(event)=>{setValue(event.target.value)}}      
+      <input type="number" 
+        placeholder = "Enter the number of Tokens you want to buy..... "
+        onChange = {(event)=>{SetnumberOfToken(event.target.value)}}    
+        min = "1"  
       />
-      <button type="submit">Submit</button>
+      <button type="submit">BUY</button>
     </form>
+    <p>Tokens Sold : {tokensSold}</p>
+    <p>Tokens left :{totalTokens}</p>
+    <p>Your address : {accounts} </p>
   </div>
   )
 
