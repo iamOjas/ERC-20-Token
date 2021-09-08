013cd92ec33af6eff4f2973b933411e18bde5feb
iamOjas <ojasmishra01@gmail.com>
2021-09-08 17:20:46 +0530

    Final Commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,6 +5,18 @@ module.exports = function (deployer) {
   deployer.deploy(BPTToken,10000000).then(function () {
     // token price is 0.00000000000001 ether
     var tokenPrice = 10000;
-    return deployer.deploy(BPTTokenSale, BPTToken.address, tokenPrice)
+      return deployer.deploy(BPTTokenSale, BPTToken.address, tokenPrice)
+    
   });
 };
+
+// module.exports = async function (deployer) {
+//   deployer.deploy(BPTToken,10000000)
+//   const token = await BPTToken.deployed();
+//   var tokenPrice = 10000;
+//   deployer.deploy(BPTTokenSale,token.address,tokenPrice);
+//   const sale = await BPTTokenSale.deployed();
+
+//   await token.transfer(sale.address,10000)
+  
+// };
